325ecdde7cdb65dd2f26ed2db3c36c41863674d7
Ramadan haider <ramadanhaider@v1041-wn-rt-b-245-112.campus-dynamic.uwaterloo.ca>
2024-05-06 18:47:04 -0400

    v1.0

---
+++ inventory_management/dashboard/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView, View
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+@login_required
+def index(request):
+    return render(request, 'dashboard/index.html')
+@login_required
+def staff(request):
+    return render(request, 'dashboard/staff.html')
+@login_required
+def product(request):
+    return render(request, 'dashboard/product.html')
+@login_required
+def order(request):
+    return render(request, 'dashboard/order.html')
